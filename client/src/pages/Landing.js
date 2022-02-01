9b4c3f31c4f4ac310fcbc786fd23ae9ad86904e7
athanadrian <athanadrian@gmail.com>
2022-02-01 16:37:43 +0200

    test

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -12,7 +12,7 @@ const Landing = () => {
       <div className='container page'>
         <div className='info'>
           <h1>
-            food <span>touring</span> app
+            food <span>tourings</span> app
           </h1>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa vel
