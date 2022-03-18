7fca41382f24dc71d323300f8710c46193e918e5
athanadrian <athanadrian@gmail.com>
2022-03-18 08:17:30 +0200

    fix helmet

--- server.js
+++ server.js
@@ -61,8 +61,14 @@ app.use(
   helmet.contentSecurityPolicy({
     useDefaults: true,
     directives: {
-      'script-src': ["'self'", "'unsafe-inline'", 'example.com'],
-      'img-src': ["'self'", 'data:', 'https://res.cloudinary.com'],
+      'default-src': ["'self'"],
+      'base-uri': ["'self'"],
+      'font-src': ["'self'", 'https:', 'data:'],
+      'frame-ancestors': ["'self'"],
+      'img-src': ["'self'", 'data:', 'http://res.cloudinary.com'],
+      'script-src': ["'self'"],
+      'script-src-attr': ["'none'"],
+      'style-src': ["'self'", 'https:', "'unsafe-inline'"],
     },
   })
 );
