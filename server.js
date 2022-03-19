18eb34e690308e138c5c4e27631195da5b3094ba
athanadrian <athanadrian@gmail.com>
2022-03-19 10:27:02 +0200

    fix

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.use(
       'base-uri': ["'self'"],
       'font-src': ["'self'", 'https:', 'data:'],
       'frame-ancestors': ["'self'"],
-      'img-src': ["'self'", 'data:', 'http://res.cloudinary.com'],
+      'img-src': ["'self'", 'data:', 'https://res.cloudinary.com'],
       'script-src': ["'self'"],
       'script-src-attr': ["'none'"],
       'style-src': ["'self'", 'https:', "'unsafe-inline'"],
